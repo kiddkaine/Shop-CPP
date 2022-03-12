2464a326694f52f121dcc7d40db49c4fe46ea500
Kidd Kaine <79826408+kiddkaine@users.noreply.github.com>
2022-03-12 15:23:15 +0500

    Update magazine.cpp

--- magazine.cpp
+++ magazine.cpp
@@ -1,38 +1,76 @@
 #include <iostream>
 using namespace std;
 
+void priceTag ()
+{
+    double chocolateSum = 0.75;
+    double coffeeSum = 0.37;
+    double beerSum = 0.56;
+    
+    cout << "* Цены на товары *" << endl;
+    cout << "Шоколад: " << chocolateSum << "$" << endl;
+    cout << "Кофе: " << coffeeSum << "$" << endl;
+    cout << "Пиво: " << beerSum << "$" << endl;
+    cout << endl;
+}
+
+void sumAmount (double productOne, double productTwo, double productThree)
+{
+    double chocolateSum = 0.75;
+    double coffeeSum = 0.37;
+    double beerSum = 0.56;
+
+    double sum = (productOne * chocolateSum) + (productTwo * coffeeSum) + (productThree * beerSum);
+    
+    cout << endl;
+    
+    cout << "Спасибо за покупку!" << endl;
+    
+    cout << "Итого: " << sum << "$" << endl;
+}
+
 int main()
 {
     setlocale(LC_ALL, "rus");
     
     cout << "*** Магазин у Миши ***" << endl << endl;
     
-    // Количество упаковок
+    priceTag();
     
     int chocolate;
-    cout << "Сколько шоколада: ";
+    cout << "Количество шоколада: ";
     cin >> chocolate;
     
     int coffee;
-    cout << "Сколько кофе: ";
+    cout << "Количество кофе: ";
     cin >> coffee;
     
     int beer;
-    cout << "Сколько пива: ";
+    cout << "Количество пива: ";
     cin >> beer;
     
-    // Цены на товары
+    if (beer > 0)
+    {
+        cout << endl;
+        
+        cout << "Вам есть 18 лет?" << endl;
+        
+        string say;
+        cin >> say;
+        
+        if (say == "Да")
+        {
+            sumAmount(chocolate, coffee, beer);
+        }
+        else
+        {
+            cout << "Отказано в продаже" << endl;
+        }
+    }
+    else
+    {
+        sumAmount(chocolate, coffee, beer);
+    }
     
-    double chocolateSum = 11.99;
-    double coffeeSum = 5.99;
-    double beerSum = 17.99;
-    
-    // Подсчёт суммы товаров;
-    
-    double sum = (chocolate * chocolateSum) + (coffee * coffeeSum) + (beer * beerSum);
-    
-    cout << endl;
-    
-    cout << "Итого: " << sum << "$" << endl;
     return 0;
 }
